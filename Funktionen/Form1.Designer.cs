850afab4ca1c569b6a20fc930c775b79bba2142f
2L0sT4u <dennis.duergerow@gmail.com>
2022-02-03 18:59:01 +0100

    Projektdateien hinzufügen.

---
+++ Funktionen/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Funktionen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.endeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtbxY2 = new System.Windows.Forms.TextBox();
+            this.txtbxX2 = new System.Windows.Forms.TextBox();
+            this.txtbxY1 = new System.Windows.Forms.TextBox();
+            this.txtbxX1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblM = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblSy = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblSx = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblFktGl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.clearToolStripMenuItem,
+            this.endeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 29);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(53, 25);
+            this.startToolStripMenuItem.Text = "start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(55, 25);
+            this.clearToolStripMenuItem.Text = "clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // endeToolStripMenuItem
+            // 
+            this.endeToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
+            this.endeToolStripMenuItem.Name = "endeToolStripMenuItem";
+            this.endeToolStripMenuItem.Size = new System.Drawing.Size(56, 25);
+            this.endeToolStripMenuItem.Text = "ende";
+            this.endeToolStripMenuItem.Click += new System.EventHandler(this.endeToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.txtbxY2);
+            this.groupBox1.Controls.Add(this.txtbxX2);
+            this.groupBox1.Controls.Add(this.txtbxY1);
+            this.groupBox1.Controls.Add(this.txtbxX1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(577, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 126);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Eingabe";
+            // 
+            // txtbxY2
+            // 
+            this.txtbxY2.BackColor = System.Drawing.SystemColors.Control;
+            this.txtbxY2.Location = new System.Drawing.Point(109, 76);
+            this.txtbxY2.Name = "txtbxY2";
+            this.txtbxY2.Size = new System.Drawing.Size(41, 26);
+            this.txtbxY2.TabIndex = 5;
+            // 
+            // txtbxX2
+            // 
+            this.txtbxX2.BackColor = System.Drawing.SystemColors.Control;
+            this.txtbxX2.Location = new System.Drawing.Point(47, 77);
+            this.txtbxX2.Name = "txtbxX2";
+            this.txtbxX2.Size = new System.Drawing.Size(46, 26);
+            this.txtbxX2.TabIndex = 4;
+            // 
+            // txtbxY1
+            // 
+            this.txtbxY1.BackColor = System.Drawing.SystemColors.Control;
+            this.txtbxY1.Location = new System.Drawing.Point(109, 35);
+            this.txtbxY1.Name = "txtbxY1";
+            this.txtbxY1.Size = new System.Drawing.Size(41, 26);
+            this.txtbxY1.TabIndex = 3;
+            // 
+            // txtbxX1
+            // 
+            this.txtbxX1.BackColor = System.Drawing.SystemColors.Control;
+            this.txtbxX1.Location = new System.Drawing.Point(47, 36);
+            this.txtbxX1.Name = "txtbxX1";
+            this.txtbxX1.Size = new System.Drawing.Size(46, 26);
+            this.txtbxX1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "P2(         /        )";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "P1(         /        )";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox2.Controls.Add(this.lblM);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.lblSy);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.lblSx);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.lblFktGl);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(577, 159);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 390);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ausgabe";
+            // 
+            // lblM
+            // 
+            this.lblM.AutoSize = true;
+            this.lblM.BackColor = System.Drawing.SystemColors.Control;
+            this.lblM.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblM.Location = new System.Drawing.Point(3, 329);
+            this.lblM.Name = "lblM";
+            this.lblM.Size = new System.Drawing.Size(109, 25);
+            this.lblM.TabIndex = 8;
+            this.lblM.Text = "m = Δy/Δx";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 304);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Anstieg:";
+            // 
+            // lblSy
+            // 
+            this.lblSy.AutoSize = true;
+            this.lblSy.BackColor = System.Drawing.SystemColors.Control;
+            this.lblSy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSy.Location = new System.Drawing.Point(3, 241);
+            this.lblSy.Name = "lblSy";
+            this.lblSy.Size = new System.Drawing.Size(104, 25);
+            this.lblSy.TabIndex = 6;
+            this.lblSy.Text = "Sy( 0 / y )";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Control;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(197, 25);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Schnittpkt y-Achse:";
+            // 
+            // lblSx
+            // 
+            this.lblSx.AutoSize = true;
+            this.lblSx.BackColor = System.Drawing.SystemColors.Control;
+            this.lblSx.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSx.Location = new System.Drawing.Point(3, 149);
+            this.lblSx.Name = "lblSx";
+            this.lblSx.Size = new System.Drawing.Size(104, 25);
+            this.lblSx.TabIndex = 4;
+            this.lblSx.Text = "Sx( x / 0 )";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(197, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Schnittpkt x-Achse:";
+            // 
+            // lblFktGl
+            // 
+            this.lblFktGl.AutoSize = true;
+            this.lblFktGl.BackColor = System.Drawing.SystemColors.Control;
+            this.lblFktGl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFktGl.Location = new System.Drawing.Point(3, 58);
+            this.lblFktGl.Name = "lblFktGl";
+            this.lblFktGl.Size = new System.Drawing.Size(131, 25);
+            this.lblFktGl.TabIndex = 2;
+            this.lblFktGl.Text = "f(x) = mx + n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Fktgleichung: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Funktionen";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem endeToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtbxY2;
+        private System.Windows.Forms.TextBox txtbxX2;
+        private System.Windows.Forms.TextBox txtbxY1;
+        private System.Windows.Forms.TextBox txtbxX1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblFktGl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblM;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblSy;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblSx;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ Funktionen/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Funktionen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        Pen p = new Pen(Color.Black, 1);
+        Font f = new Font("Comic Sans MS", 8);
+
+        private void endeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void DrawCoordinateSystem()
+        {
+            g = CreateGraphics();
+            g.DrawLine(p, 50, 300, 550, 300);
+            g.DrawLine(p, 300, 50, 300, 550);
+
+            //Markierungen für Einheiten zeichnen
+            int a = 50;
+            int b = -4;
+            int bb = 4;
+            for (int i = 0; i < 9; i++)
+            {
+                a += 50;
+                g.DrawLine(p, a, 305, a, 295);
+                g.DrawLine(p, 305, a, 295, a);
+
+                if (b != 0)
+                {
+                    g.DrawString(b.ToString(), f, Brushes.Black, a - 5, 305);
+                    g.DrawString(bb.ToString(), f, Brushes.Black, 305, a - 9);
+                }
+                b += 1;
+                bb -= 1;
+            }
+        }
+        private void DrawFunctionGraph(float x1, float y1, float x2 ,float y2)
+        {
+            float m = (y2 - y1) / (x2 - x1);
+            float n = y1 - (x1 * m);
+
+            float X1 = -250;
+            float Y1 = X1 * m + n * 50;
+            float X2 = 250;
+            float Y2 = X2*m+n*50;
+            
+            Graphics g;
+            Pen p = new Pen(Color.Red, 3);
+
+            do
+            {
+                Y1 = X1 * m + n * 50;
+                X1 += 1;
+            }
+            while (Y1 >= 250 || Y1 <= -250);
+
+            do
+            {
+                Y2 = X2 * m + n * 50;
+                X2 -= 1;
+            }
+            while (Y2 >= 250 || Y2 <= -250);
+
+            g = CreateGraphics();
+            g.TranslateTransform(300, 300);
+            g.DrawLine(p, X1, -1*Y1, X2, -1*Y2);
+        }
+        private void CalcFunction(Double x1, Double y1, Double x2, Double y2)
+        {
+            Double m, n, x0;
+
+            m = (y2 - y1) / (x2 - x1);
+            n = y1 - x1 * m;
+            x0 = -n / m;
+
+            if (y1 == y2)
+            {
+                lblFktGl.Text = "f(x)= " + Math.Round(Convert.ToDouble(txtbxY1.Text), 2).ToString();
+                lblSy.Text = "Sy(0 / " + Math.Round(Convert.ToDouble(txtbxY1.Text), 2).ToString() + " )";
+                lblSx.Text = "-";
+                lblM.Text = "m = -";
+            }
+            else if (x1 == x2)
+            {
+            }
+            else
+            {
+                lblFktGl.Text = "f(x)= " + Math.Round(m, 2).ToString() + "x + " + Math.Round(n, 2).ToString();
+                lblSy.Text = "Sy(0 / " + Math.Round(n, 2).ToString() + " )";
+                lblSx.Text = "Sx( " + Math.Round(x0, 2).ToString() + " / 0 )";
+                lblM.Text = "m = " + Math.Round(m, 2).ToString();
+            }
+        }
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DrawCoordinateSystem(); //Koorddinatensystem zeichnen
+            CalcFunction(Convert.ToDouble(txtbxX1.Text), Convert.ToDouble(txtbxY1.Text), Convert.ToDouble(txtbxX2.Text), Convert.ToDouble(txtbxY2.Text));
+            //Sx, Sy, Anstieg und Funktionsgleichung berechnen
+            Thread.Sleep(500);
+
+            if (float.Parse(txtbxX1.Text) == float.Parse(txtbxX2.Text)) // Fehlermneldung wenn beide Punkte dasselbe X haben
+            {
+                if (float.Parse(txtbxY1.Text) == float.Parse(txtbxY2.Text)){
+                    MessageBox.Show("Die Punkte dürfen nicht identisch sein", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }   //Fehlermeldung wenn Punkte identisch sind
+                else{
+                    MessageBox.Show("Die Punkte müssen verschiedene X-Werte haben", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                } }  // Fehlermneldung wenn beide Punkte dasselbe X haben
+            else
+            {
+                DrawFunctionGraph(float.Parse(txtbxX1.Text), float.Parse(txtbxY1.Text), float.Parse(txtbxX2.Text), float.Parse(txtbxY2.Text));
+            }   //Funktion zeichnen [-4,5; 4,5]
+        }
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.Clear(BackColor);
+        }
+    }
+}
